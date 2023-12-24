daac22c1bb6f9ff8a1c35c87fd2259111fbcd51c
Muhammad Shazail Sohail <19736624@student.curtin.edu.au>
2023-12-24 23:11:15 +0500

    updated vercel

--- rapidCare/rapidCare/settings.py
+++ rapidCare/rapidCare/settings.py
@@ -163,15 +163,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_URL = "static/"
 
-# STATIC_DIRS = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # For Whitenoise to work in the production
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = "static/"
 
 
 # Default primary key field type
