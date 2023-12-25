113665c6e7fabeb9d97dcdd7b94fb6189f2773ae
Muhammad Shazail Sohail <19736624@student.curtin.edu.au>
2023-12-25 18:54:43 +0500

    changing DB to postgres

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+echo "Building Project"
+
+# Installing Necessary Packages
 pip install -r requirements.txt
+
+# creating DB for Django
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+
+# Adding CSS and IMAGES to static folder
 python3.9 manage.py collectstatic
+
+echo "Build Complete"
--- rapidCare/settings.py
+++ rapidCare/settings.py
@@ -127,10 +127,22 @@ WSGI_APPLICATION = "rapidCare.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'b-eDBbd3fGGG41d33FC6c6FCAEfF3aDc',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '55080',
     }
 }
 
